744ba3ae5459f23cc20d36d1fbb5a58bfd69c20a
moris96 <moriskhoudari@gmail.com>
2023-02-06 13:02:29 -0500

    repo created

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import { useState, useEffect } from "react"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Moris Portfolio!</h1>
+    </div>
+  );
+}
+
+export default App;
