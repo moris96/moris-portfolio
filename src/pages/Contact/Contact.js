a17c4869b8ed88302e8682557f0c2472b39a7b6e
moris96 <moriskhoudari@gmail.com>
2023-02-07 13:31:19 -0500

    working on contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -5,9 +5,10 @@ export default function Contact(){
         <>
         <div className="about-me">
             <h1 className="get-in-touch">Get in touch</h1>
-            <p>If you want to talk to me, collaborate on a project, or simply say hi, fill the form below!</p>
+            <p className="lets-talk">If you want to talk to me, collaborate on a project, or simply say hi, send an email to <a href="mailto:moriskhoudari@gmail.com">moriskhoudari@gmail</a>. Let's talk!</p>
         </div>
 
+        <h1 className="socials">Socials</h1>
         <div className="socials">
             <button className="linkden-btn"><img className="linkden" src="https://cdn3.iconfinder.com/data/icons/sociocons/256/linkedin-sociocon.png" alt="linkden" /></button>
             <button className="github-btn"><img className="github" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png" alt="github" /></button>
