75c543384402174051ed808ee8574962291e2d28
moris96 <moriskhoudari@gmail.com>
2023-02-09 14:03:31 -0500

    updated projects page styles

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -4,12 +4,20 @@ export default function Projects(){
     return(
         <>
             <div className="projects-page">
-                <br/><a href="https://moris96.github.io/tower-defense-game/index.html" target="_blank">p1</a>
-                <br/><img className="project1" src={process.env.PUBLIC_URL+"/images/project1.png"}/>
-                
-                <br/><a href="https://tumble.herokuapp.com/" target="_blank">group tumblr</a>
-                <br/><a href="https://timely-lollipop-c213d5.netlify.app/" target="_blank">capstone</a>
+                <h1 className="portfolio-pg">web developer portfolio</h1>
+                <h3 className="portfolio-expl">From vanilla JS to full stack web apps using React.js, Node.js, MongoDB, Express.js; and building games with HTML canvas!</h3>
             </div>
+
+            <div className="projs-names">
+                <a className="p1" href="https://moris96.github.io/tower-defense-game/index.html" target="_blank">Game: Soldiers vs Meme Zombies!</a>
+                <a className="p2" href="https://tumble.herokuapp.com/" target="_blank">Tumble: tumblr.com clone</a>
+                <a className="p3" href="https://timely-lollipop-c213d5.netlify.app/" target="_blank">Game: Pokemon Game Clone</a>
+            </div>
+            
+            
+            <img className="project1" alt="proj1" src={process.env.PUBLIC_URL+"/images/project1.png"}/>
+            <img className="groupProject" alt="groupproj" src={process.env.PUBLIC_URL+"/images/tumble.png"}/>
+            <img className="capstone" alt="capstoneproj" src={process.env.PUBLIC_URL+"/images/poke.png"}/>
             
         </>
     )
