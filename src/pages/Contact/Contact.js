1314d04855b5e978eddc0cdfddc977ff70969d47
moris96 <moriskhoudari@gmail.com>
2023-04-20 14:12:15 -0400

    updated scss styles for contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,24 +1,19 @@
-
+import styles from "./Contact.module.scss";
 
 export default function Contact(){
     return(
         <>
-        <div className="contact-me">
-            <h1 className="get-in-touch">Get in touch</h1>
-            <p className="lets-talk">If you want to talk to me, collaborate on a project, or simply say hi, send an email to <a href="mailto:moriskhoudari@gmail.com">moriskhoudari@gmail</a>. Let's talk!</p>
+        <div className={styles.contactMe}>
+            <h1 className={styles.getInTouch}>Get in touch</h1>
+            <p className={styles.letsTalk}>If you want to talk to me, collaborate on a project, or simply say hi, send an email to <a href="mailto:moriskhoudari@gmail.com">moriskhoudari@gmail</a>. Let's talk!</p>
         </div>
 
-        <h1 className="socials">Socials</h1>
-        <div className="socials">
+        <h1 className={styles.socials}>Socials</h1>
+        <div className={styles.socials}>
             <a className="linkden" href="https://www.linkedin.com/in/moris-khoudari-abady/" target="_blank">linkedin</a><br/>
             <br/><a className="github" href="https://github.com/moris96" target="_blank">Github</a>
 
-
-{/* 
-            
-            <button className="linkden-btn"><img className="linkden" src="https://cdn3.iconfinder.com/data/icons/sociocons/256/linkedin-sociocon.png" alt="linkden" /></button>
-            <button className="github-btn"><img className="github" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png" alt="github" /></button> */}
         </div>
     </>
-    )
-}
+    );
+};
